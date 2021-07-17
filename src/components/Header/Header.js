58b4a5579e244ed9f55303207f075faeac5d72be
April Day <april.day@gmail.com>
2021-07-17 17:00:06 +0100

    start creating robots commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+import logo from '../../Github-Mark-Light-64px.png';
+
+    const HeaderWrapper = styled.divd`
+        background-color: #282c34;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        font-size: calc(10px + 2vmin);
+        color: white;
+        width: 100%;
+    `;
+
+    const Logo = styled.img`
+        height: 64px; 
+        pointer-events: none;
+        `;
+    
+    const Header = () => (
+        <HeaderWrapper>
+            <Logo src={`https://robohash.org/7>size=200x200`} alt ='logo' />
+            <h1>My robot friends</h1>
+        </HeaderWrapper>
+    )
+
+export default Header;
